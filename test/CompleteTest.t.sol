09614cdef73923c2895b465403000ef96f5dcb8a
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-11-17 15:23:00 +0000

    complete

--- test/CompleteTest.t.sol
+++ test/CompleteTest.t.sol
@@ -18,14 +18,21 @@ contract CompleteSystemTest is Test {
     address charlie = makeAddr("charlie");
     address dave = makeAddr("dave");
 
-    // Set up the initial balances for ETH and tokens for testing
+    // Set up the initial balances for ETH and tokens for testing (in wei)
     uint256 initialBalance = 10 ether;
 
-    // Define ticket prices for the primary market
+    // Define ticket prices for the primary market (in wei)
     uint256 ticketPrice = 1 ether;
     uint256 bobListPrice = 2 ether;
     uint256 daveBidPrice = 3 ether;
 
+    uint256 initialPT = 100 ether;
+
+    uint256 alicePTbalance = 0 ether;
+    uint256 bobPTbalance = 0 ether;
+    uint256 charliePTbalance = 0 ether;
+    uint256 davePTbalance = 0 ether;
+    
     function setUp() public {
         purchaseToken = new PurchaseToken();
         primaryMarket = new PrimaryMarket(purchaseToken);
@@ -37,21 +44,25 @@ contract CompleteSystemTest is Test {
         vm.deal(charlie, initialBalance);
         vm.deal(dave, initialBalance);
 
-        // Mint tokens for users
+        // Mint tokens for users (the mint function expects ether and returns 100 times the tokens in wei)
         vm.startPrank(alice);
-        purchaseToken.mint{value: initialBalance}();
+        purchaseToken.mint{value: 1 ether}(); // Alice gets 100 PT
+        alicePTbalance = purchaseToken.balanceOf(alice);
         vm.stopPrank();
 
         vm.startPrank(bob);
-        purchaseToken.mint{value: initialBalance}();
+        purchaseToken.mint{value: 1 ether}(); // Bob gets 100 PT
+        bobPTbalance = purchaseToken.balanceOf(bob);
         vm.stopPrank();
 
         vm.startPrank(charlie);
-        purchaseToken.mint{value: initialBalance}();
+        purchaseToken.mint{value: 1 ether}(); // Charlie gets 100 PT
+        charliePTbalance = purchaseToken.balanceOf(charlie);
         vm.stopPrank();
 
         vm.startPrank(dave);
-        purchaseToken.mint{value: initialBalance}();
+        purchaseToken.mint{value: 1 ether}(); // Dave gets 100 PT
+        davePTbalance = purchaseToken.balanceOf(dave);
         vm.stopPrank();
     }
 
@@ -59,7 +70,6 @@ contract CompleteSystemTest is Test {
         // Alice creates a new event on the primary market
         vm.startPrank(alice);
         ITicketNFT ticketNFT = primaryMarket.createNewEvent("Live Concert", ticketPrice, 100);
-        uint256 aliceTokenBalanceAfterEvent = purchaseToken.balanceOf(alice);
         vm.stopPrank();
 
         // Bob purchases a ticket for the event
@@ -95,15 +105,24 @@ contract CompleteSystemTest is Test {
         uint256 bobTokenBalanceAfterSale = purchaseToken.balanceOf(bob);
         vm.stopPrank();
 
-        // Assert final balances and ticket ownership
-        assertEq(ticketNFT.holderOf(ticketIdBob), dave, "Dave should now own the ticket");
-        assertEq(ticketNFT.holderOf(ticketIdCharlie), charlie, "Charlie should still own his ticket");
-
+        // Calculate the fee in wei
         uint256 feeAmount = (daveBidPrice * 5) / 100;
-        uint256 bobFinalBalance = initialBalance - ticketPrice + daveBidPrice - feeAmount;
-        uint256 daveFinalBalance = initialBalance - daveBidPrice;
-        uint256 aliceFinalBalance = initialBalance + ticketPrice + ticketPrice + feeAmount;
-        assertEq(purchaseToken.balanceOf(charlie), charlieTokenBalanceAfterPurchase, "Charlie's balance should remain unchanged after the market activities");
 
-        }
+        // Bob's final balance should be his initial balance minus the ticket price plus Dave's bid price minus the fee
+        uint256 bobFinalBalance = bobPTbalance - ticketPrice + daveBidPrice - feeAmount;
+
+        // Dave's final balance should be his initial balance minus Dave's bid price
+        uint256 daveFinalBalance = davePTbalance - daveBidPrice;
+
+        // Alice's final balance should be her initial balance plus the ticket price from the primary market sale plus the fee from the secondary market sale
+        uint256 aliceFinalBalance = alicePTbalance + ticketPrice + ticketPrice + feeAmount;
+
+        // Charlie's final balance should remain unchanged after his initial purchase
+        uint256 charlieFinalBalance = charliePTbalance - ticketPrice; // Assuming Charlie bought 1 ticket in the primary market
+
+        assertEq(purchaseToken.balanceOf(alice), aliceFinalBalance, "Alice's balance should be correct after the market activities");
+        assertEq(purchaseToken.balanceOf(bob), bobFinalBalance, "Bob's balance should be correct after the market activities");
+        assertEq(purchaseToken.balanceOf(dave), daveFinalBalance, "Dave's balance should be correct after the market activities");
+        assertEq(purchaseToken.balanceOf(charlie), charlieFinalBalance, "Charlie's balance should remain unchanged after the market activities");
+    }
 }
